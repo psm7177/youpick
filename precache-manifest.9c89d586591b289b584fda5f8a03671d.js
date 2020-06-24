c18ecc9ff2002bdb08dccab527d5770534570023
psm7177 <psm7177@naver.com>
2020-06-25 00:21:06 +0900

    Updates

---
+++ precache-manifest.9c89d586591b289b584fda5f8a03671d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856d37ff599312468dd6066ee0a6b438",
+    "url": "/youpick/index.html"
+  },
+  {
+    "revision": "73b0ffa0ca49bb3c3f9c",
+    "url": "/youpick/static/css/main.c90f6068.chunk.css"
+  },
+  {
+    "revision": "a8417f38fc29b7567c2b",
+    "url": "/youpick/static/js/2.7825648e.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/youpick/static/js/2.7825648e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b0ffa0ca49bb3c3f9c",
+    "url": "/youpick/static/js/main.e4ba89ea.chunk.js"
+  },
+  {
+    "revision": "4fc1162f4504e828e0d1",
+    "url": "/youpick/static/js/runtime-main.1fa21b62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youpick/static/media/logo.5d5d9eef.svg"
+  }
+]);
